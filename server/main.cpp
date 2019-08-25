f7a865e0d365d3731f40feae6e961db8148eb2af
Manfred Kalan <manfred.kalan@gmail.com>
2019-08-25 20:47:48 +0200

    add websocket

--- server/main.cpp
+++ server/main.cpp
@@ -3,6 +3,7 @@
 #include<spdlog/spdlog.h>
 #include<spdlog/sinks/stdout_color_sinks.h>
 #include <drogon/HttpController.h>
+#include <drogon/WebSocketController.h>
 #include <drogon/HttpAppFramework.h>
 auto console = spdlog::stdout_color_mt("main");
 using namespace drogon;
@@ -13,6 +14,7 @@ namespace demo {
             METHOD_LIST_BEGIN
                 //list path definitions here;
                 //PATH_ADD("/path","filter1","filter2",HttpMethod1,HttpMethod2...);
+                // http http://127.0.0.1:8088/demo/v1/User/get/1
                 METHOD_ADD(User::getter, "get/{1}", Get);
             METHOD_LIST_END
 
@@ -28,6 +30,30 @@ namespace demo {
         };
     }
 }
+class EchoWebsock : public drogon::WebSocketController<EchoWebsock> {
+public:
+    WS_PATH_LIST_BEGIN
+        // list path definations here;
+        WS_PATH_ADD("/echo");
+    WS_PATH_LIST_END
+
+    void handleNewMessage(const WebSocketConnectionPtr &ptr, std::string &&msg,
+                          const WebSocketMessageType &type) override {
+        LOG_DEBUG << "new websocket message" << msg;
+
+    }
+
+    void handleNewConnection(const HttpRequestPtr &ptr, const WebSocketConnectionPtr &connectionPtr) override {
+        LOG_DEBUG << "new connection ";
+        LOG_DEBUG << connectionPtr->peerAddr().toIp();
+        connectionPtr->send("Hello");
+    }
+
+    void handleConnectionClosed(const WebSocketConnectionPtr &ptr) override {
+        LOG_DEBUG << "connection closed";
+    }
+};
+
 int main(int argc, char* argv[]) {
 	int16_t port = 8088;
     bool help = false;
@@ -46,7 +72,7 @@ int main(int argc, char* argv[]) {
     }
     spdlog::get("main")->info("begin");
     spdlog::get("main")->info(fmt::format("running at port={}",port));
-    // drogon::app().addListener("0.0.0.0",port);
+    //drogon::app().addListener("0.0.0.0",port);
     drogon::app().loadConfigFile("../etc/config.json");
     drogon::app().run();
 	spdlog::get("main")->info("end");
